27e9b98cdf15e228871e3130798efb05ccfa7468
Vishalgv3 <vishalcanada03@gmail.com>
2024-01-11 20:26:42 -0400

    added THREE into the project folder

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
 
-// JS content goes here
-// ...
+import * as THREE from "three";
 
 function main() {
-    let message = "Hello world!";
 
-    document.getElementById("message2").innerHTML = message;
-    document.getElementById("message1").innerHTML = "My first JS program...";
-    document.getElementById("message3").innerHTML = "HEHEHEHEHEHEHEH.....";
-    console.log("main function did this");
 }
 
 main();
